7fd652303a56f7402984cea91df143d9fdd37e92
Valentin Fries <contact@fries.io>
2018-11-14 11:52:13 +0100

    Early return sellIn and quality update for Sulfuras

--- src/main/java/com/gildedrose/InventoryItem.java
+++ src/main/java/com/gildedrose/InventoryItem.java
@@ -18,6 +18,9 @@ class InventoryItem {
     }
 
     private void updateQuality() {
+        if (item.name.equals("Sulfuras, Hand of Ragnaros"))
+            return;
+
         if (item.name.equals("Aged Brie")
                 || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             if (item.quality < 50) {
@@ -39,19 +42,16 @@ class InventoryItem {
             }
         } else {
             if (item.quality > 0) {
-                if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                } else {
-                    item.quality = item.quality - 1;
-                }
+                item.quality = item.quality - 1;
             }
         }
     }
 
     private void updateSellIn() {
-        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-        } else {
-            item.sellIn = item.sellIn - 1;
-        }
+        if (item.name.equals("Sulfuras, Hand of Ragnaros"))
+            return;
+
+        item.sellIn = item.sellIn - 1;
 
         if (item.sellIn < 0) {
             if (item.name.equals("Aged Brie")) {
@@ -63,9 +63,7 @@ class InventoryItem {
                     item.quality = 0;
                 } else {
                     if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.quality = item.quality - 1;
-                        }
+                        item.quality = item.quality - 1;
                     }
                 }
             }
