0e733f8c6251fa5d9214e8d4ceac8ea5fbd33ad2
Valentin Fries <contact@fries.io>
2018-11-14 09:54:07 +0100

    JUnit 5 Jupiter API migration

--- src/test/java/com/gildedrose/GildedRoseAcceptanceTest.java
+++ src/test/java/com/gildedrose/GildedRoseAcceptanceTest.java
@@ -1,13 +1,17 @@
 package com.gildedrose;
 
-import org.junit.Test;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class GildedRoseAcceptanceTest {
+@DisplayName("When I update the inventory it should")
+class GildedRoseAcceptanceTest {
 
     @Test
-    public void should_decrease_the_item_quality_when_the_inventory_is_updated() {
+    @DisplayName("decrease the item quality")
+    void should_decrease_the_item_quality_when_the_inventory_is_updated() {
         final Item item = new Item("Dummy", 1, 1);
         final Item[] items = new Item[]{item};
         final GildedRose gildedRose = new GildedRose(items);
@@ -18,7 +22,8 @@ public class GildedRoseAcceptanceTest {
     }
 
     @Test
-    public void should_decrease_the_item_sellin_when_the_inventory_is_updated() {
+    @DisplayName("decease the item sellIn")
+    void should_decrease_the_item_sellin_when_the_inventory_is_updated() {
         final Item item = new Item("Dummy", 1, 1);
         final Item[] items = new Item[]{item};
         final GildedRose gildedRose = new GildedRose(items);
@@ -29,7 +34,8 @@ public class GildedRoseAcceptanceTest {
     }
 
     @Test
-    public void should_decrease_the_item_quality_twice_when_the_inventory_is_updated_and_the_item_sellin_is_negative() {
+    @DisplayName("decrease the item quality twice when its sellIn is negative")
+    void should_decrease_the_item_quality_twice_when_the_inventory_is_updated_and_the_item_sellin_is_negative() {
         final Item item = new Item("Dummy", 0, 4);
         final Item[] items = new Item[]{item};
         final GildedRose gildedRose = new GildedRose(items);
--- src/test/java/com/gildedrose/ItemTest.java
+++ src/test/java/com/gildedrose/ItemTest.java
@@ -1,13 +1,16 @@
 package com.gildedrose;
 
-import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class ItemTest {
+@DisplayName("Item should")
+class ItemTest {
 
     @Test
-    public void should_print_the_item_name_sellin_and_quality_separated_by_a_comma_and_a_space() {
+    @DisplayName("print its name, sellIn and quality separated by a comma and a space")
+    void should_print_the_item_name_sellin_and_quality_separated_by_a_comma_and_a_space() {
         final Item item = new Item("Dummy", 3, 7);
 
         final String result = item.toString();
