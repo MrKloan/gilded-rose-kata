178cfea976b3a8e06d5e0437ba7c7952235912e8
Valentin Fries <contact@fries.io>
2018-11-14 09:48:24 +0100

    Should decrease the item quality twice when the inventory is updated and the item sellIn is negative.

--- src/test/java/com/gildedrose/GildedRoseAcceptanceTest.java
+++ src/test/java/com/gildedrose/GildedRoseAcceptanceTest.java
@@ -27,4 +27,15 @@ public class GildedRoseAcceptanceTest {
 
         assertThat(gildedRose.getItemSellIn(0)).isEqualTo(0);
     }
+
+    @Test
+    public void should_decrease_the_item_quality_twice_when_the_inventory_is_updated_and_the_item_sellin_is_negative() {
+        final Item item = new Item("Dummy", 0, 4);
+        final Item[] items = new Item[]{item};
+        final GildedRose gildedRose = new GildedRose(items);
+
+        gildedRose.updateQuality();
+
+        assertThat(gildedRose.getItemQuality(0)).isEqualTo(2);
+    }
 }
