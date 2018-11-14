0c715f659855bf35413d9093b2874d2cf7860b6c
Valentin Fries <contact@fries.io>
2018-11-14 10:41:34 +0100

    Refactored tests by extracting a withInventoryContaining method to initialize the Gilded Rose inventory with 1 Item

--- src/test/java/com/gildedrose/GildedRoseAcceptanceTest.java
+++ src/test/java/com/gildedrose/GildedRoseAcceptanceTest.java
@@ -12,9 +12,7 @@ class GildedRoseAcceptanceTest {
     @Test
     @DisplayName("decease the item sellIn")
     void should_decrease_the_item_sellin_when_the_inventory_is_updated() {
-        final Item item = new Item("Dummy", 1, 1);
-        final Item[] items = new Item[]{item};
-        final GildedRose gildedRose = new GildedRose(items);
+        final GildedRose gildedRose = withInventoryContaining("Dummy", 1, 1);
 
         gildedRose.updateQuality();
 
@@ -24,9 +22,7 @@ class GildedRoseAcceptanceTest {
     @Test
     @DisplayName("decrease the item quality")
     void should_decrease_the_item_quality_when_the_inventory_is_updated() {
-        final Item item = new Item("Dummy", 1, 1);
-        final Item[] items = new Item[]{item};
-        final GildedRose gildedRose = new GildedRose(items);
+        final GildedRose gildedRose = withInventoryContaining("Dummy", 1, 1);
 
         gildedRose.updateQuality();
 
@@ -36,9 +32,7 @@ class GildedRoseAcceptanceTest {
     @Test
     @DisplayName("decrease the item quality twice when its sellIn is negative")
     void should_decrease_the_item_quality_twice_when_the_inventory_is_updated_and_the_item_sellin_is_negative() {
-        final Item item = new Item("Dummy", 0, 3);
-        final Item[] items = new Item[]{item};
-        final GildedRose gildedRose = new GildedRose(items);
+        final GildedRose gildedRose = withInventoryContaining("Dummy", 0, 3);
 
         gildedRose.updateQuality();
 
@@ -48,9 +42,7 @@ class GildedRoseAcceptanceTest {
     @Test
     @DisplayName("not decrease the item quality below 0")
     void should_not_decrease_the_item_quality_below_zero_when_the_inventory_is_updated() {
-        final Item item = new Item("Dummy", 3, 0);
-        final Item[] items = new Item[]{item};
-        final GildedRose gildedRose = new GildedRose(items);
+        final GildedRose gildedRose = withInventoryContaining("Dummy", 3, 0);
 
         gildedRose.updateQuality();
 
@@ -60,9 +52,7 @@ class GildedRoseAcceptanceTest {
     @Test
     @DisplayName("increase the quality of Aged Brie when its sellIn is positive")
     void should_increase_the_quality_of_aged_brie_when_the_inventory_is_updated_and_its_sellin_is_positive() {
-        final Item item = new Item("Aged Brie", 3, 6);
-        final Item[] items = new Item[]{item};
-        final GildedRose gildedRose = new GildedRose(items);
+        final GildedRose gildedRose = withInventoryContaining("Aged Brie", 3, 6);
 
         gildedRose.updateQuality();
 
@@ -72,9 +62,7 @@ class GildedRoseAcceptanceTest {
     @Test
     @DisplayName("increase the quality of Aged Brie twice when its sellIn is negative")
     void should_increase_the_quality_of_aged_brie_twice_when_the_inventory_is_updated_and_its_sellin_is_negative() {
-        final Item item = new Item("Aged Brie", 0, 5);
-        final Item[] items = new Item[]{item};
-        final GildedRose gildedRose = new GildedRose(items);
+        final GildedRose gildedRose = withInventoryContaining("Aged Brie", 0, 5);
 
         gildedRose.updateQuality();
 
@@ -84,9 +72,7 @@ class GildedRoseAcceptanceTest {
     @Test
     @DisplayName("not increase the quality of an item above 50")
     void should_not_increase_the_quality_of_an_item_above_fifty_when_the_inventory_is_updated() {
-        final Item item = new Item("Aged Brie", 7, 50);
-        final Item[] items = new Item[]{item};
-        final GildedRose gildedRose = new GildedRose(items);
+        final GildedRose gildedRose = withInventoryContaining("Aged Brie", 7, 50);
 
         gildedRose.updateQuality();
 
@@ -96,9 +82,7 @@ class GildedRoseAcceptanceTest {
     @Test
     @DisplayName("not decrease the quality of a legendary item")
     void should_not_decrease_the_quality_of_a_legendary_item_when_the_inventory_is_updated() {
-        final Item item = new Item("Sulfuras, Hand of Ragnaros", 0, 80);
-        final Item[] items = new Item[]{item};
-        final GildedRose gildedRose = new GildedRose(items);
+        final GildedRose gildedRose = withInventoryContaining("Sulfuras, Hand of Ragnaros", 0, 80);
 
         gildedRose.updateQuality();
 
@@ -108,9 +92,7 @@ class GildedRoseAcceptanceTest {
     @Test
     @DisplayName("not decrease the sellIn of a legendary item")
     void should_not_decrease_the_sellin_of_a_legendary_item_when_the_inventory_is_updated() {
-        final Item item = new Item("Sulfuras, Hand of Ragnaros", 0, 80);
-        final Item[] items = new Item[]{item};
-        final GildedRose gildedRose = new GildedRose(items);
+        final GildedRose gildedRose = withInventoryContaining("Sulfuras, Hand of Ragnaros", 0, 80);
 
         gildedRose.updateQuality();
 
@@ -120,9 +102,7 @@ class GildedRoseAcceptanceTest {
     @Test
     @DisplayName("increase the quality of a Backstage Pass when its sellIn is above 10")
     void should_increase_the_quality_of_a_backstage_pass_when_the_inventory_is_updated_and_its_sellin_is_above_10() {
-        final Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20);
-        final Item[] items = new Item[]{item};
-        final GildedRose gildedRose = new GildedRose(items);
+        final GildedRose gildedRose = withInventoryContaining("Backstage passes to a TAFKAL80ETC concert", 15, 20);
 
         gildedRose.updateQuality();
 
@@ -132,9 +112,7 @@ class GildedRoseAcceptanceTest {
     @Test
     @DisplayName("increase the quality of a Backstage Pass twice when its sellIn is between 10 and 5")
     void should_increase_the_quality_of_a_backstage_pass_twice_when_the_inventory_is_updated_and_its_sellin_is_between_10_and_5() {
-        final Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20);
-        final Item[] items = new Item[]{item};
-        final GildedRose gildedRose = new GildedRose(items);
+        final GildedRose gildedRose = withInventoryContaining("Backstage passes to a TAFKAL80ETC concert", 10, 20);
 
         gildedRose.updateQuality();
 
@@ -144,9 +122,7 @@ class GildedRoseAcceptanceTest {
     @Test
     @DisplayName("increase the quality of a Backstage Pass thrice when its sellIn is between 5 and 0")
     void should_increase_the_quality_of_a_backstage_pass_thrice_when_the_inventory_is_updated_and_its_sellin_is_between_5_and_0() {
-        final Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20);
-        final Item[] items = new Item[]{item};
-        final GildedRose gildedRose = new GildedRose(items);
+        final GildedRose gildedRose = withInventoryContaining("Backstage passes to a TAFKAL80ETC concert", 5, 20);
 
         gildedRose.updateQuality();
 
@@ -156,12 +132,16 @@ class GildedRoseAcceptanceTest {
     @Test
     @DisplayName("drop the quality of a Backstage Pass to 0 when its sellIn negative")
     void should_drop_the_quality_of_a_backstage_pass_to_zero_when_the_inventory_is_updated_and_its_sellin_is_negative() {
-        final Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20);
-        final Item[] items = new Item[]{item};
-        final GildedRose gildedRose = new GildedRose(items);
+        final GildedRose gildedRose = withInventoryContaining("Backstage passes to a TAFKAL80ETC concert", 0, 20);
 
         gildedRose.updateQuality();
 
         assertThat(gildedRose.getItemQuality(0)).isEqualTo(0);
+    }
+
+    private GildedRose withInventoryContaining(final String name, final int sellIn, final int quality) {
+        return new GildedRose(new Item[]{
+                new Item(name, sellIn, quality)
+        });
     }
 }
