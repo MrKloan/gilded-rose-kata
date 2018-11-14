451c84bea043240196a49d2e694fafd019b47893
Valentin Fries <contact@fries.io>
2018-11-14 12:05:46 +0100

    Exploded quality and sellIn methods in multiple business methods

--- src/main/java/com/gildedrose/InventoryItem.java
+++ src/main/java/com/gildedrose/InventoryItem.java
@@ -21,30 +21,27 @@ class InventoryItem {
         if (item.name.equals("Sulfuras, Hand of Ragnaros"))
             return;
 
-        if (item.name.equals("Aged Brie")
-                || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
+        if (item.name.equals("Aged Brie") || item.name.equals("Backstage passes to a TAFKAL80ETC concert"))
+            updateImprovableItemQuality();
+        else
+            decreaseQuality();
+    }
 
-                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
+    private void updateImprovableItemQuality() {
+        if (item.quality >= 50)
+            return;
 
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-                }
-            }
-        } else {
-            if (item.quality > 0) {
-                item.quality = item.quality - 1;
-            }
-        }
+        item.quality = item.quality + 1;
+
+        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert"))
+            updateBackstagePassQuality();
+    }
+
+    private void updateBackstagePassQuality() {
+        if (item.sellIn < 11)
+            increaseQuality();
+        if (item.sellIn < 6)
+            increaseQuality();
     }
 
     private void updateSellIn() {
@@ -55,19 +52,24 @@ class InventoryItem {
 
         if (item.sellIn < 0) {
             if (item.name.equals("Aged Brie")) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
+                increaseQuality();
             } else {
-                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert"))
                     item.quality = 0;
-                } else {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
-                }
+                else
+                    decreaseQuality();
             }
         }
+    }
+
+    private void increaseQuality() {
+        if (item.quality < 50)
+            item.quality = item.quality + 1;
+    }
+
+    private void decreaseQuality() {
+        if (item.quality > 0)
+            item.quality = item.quality - 1;
     }
 
     int getSellIn() {
