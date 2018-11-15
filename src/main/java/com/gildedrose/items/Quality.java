5910ed1d1ea1c3ba5e4f374d398e3c6107fd0734
Valentin Fries <contact@fries.io>
2018-11-15 16:46:55 +0100

    Quality equals & toString

--- src/main/java/com/gildedrose/items/Quality.java
+++ src/main/java/com/gildedrose/items/Quality.java
@@ -1,9 +1,17 @@
 package com.gildedrose.items;
 
+import java.util.Objects;
+
 public class Quality {
 
+    private final int value;
+
+    private Quality(final int value) {
+        this.value = value;
+    }
+
     public static Quality of(final int value) {
-        throw new UnsupportedOperationException();
+        return new Quality(value);
     }
 
     Quality increase() {
@@ -12,5 +20,23 @@ public class Quality {
 
     Quality decrease() {
         throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Quality quality = (Quality) o;
+        return value == quality.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return "Quality: " + value;
     }
 }
